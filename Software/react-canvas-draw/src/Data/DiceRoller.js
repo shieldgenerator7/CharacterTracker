dedde1bdee45ca42dd737288c34cc7fbafb87fb8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-09-20 21:26:35 -0400

    Add file: DiceRoller.js

---
+++ Software/react-canvas-draw/src/Data/DiceRoller.js
@@ -0,0 +1,48 @@
+"use strict";
+
+class DiceRoller{
+    constructor() {
+        
+    }
+
+
+    rollDice(dieRollString) {
+        
+        if (dieRollString.contains("d")) {
+            console.error("Input must be in die roll form, ex: 1d20.", dieRollString);
+            return 0;
+        }
+        let split = dieRollString.split("d").map(v => (v.trim() || 1) * 1);
+        this._rollDice(split[0], split[1]);
+    }
+    _rollDice(count, die) {
+        let sum = 0;
+        for (let i = 0; i < count; i++){
+            let roll = Math.ceil(Math.random() * die);
+            sum += roll;
+        }
+        return sum;
+    }
+}
+export default DiceRoller;
+
+export function rollDice(dieRollString) {
+        
+    if (dieRollString.contains("d")) {
+        console.error("Input must be in die roll form, ex: 1d20.", dieRollString);
+        return 0;
+    }
+    let split = dieRollString.split("d").map(v => (v.trim() || 1) * 1);
+    _rollDice(split[0], split[1]);
+}
+function _rollDice(count, die) {
+    let sum = 0;
+    for (let i = 0; i < count; i++){
+        let roll = Math.ceil(Math.random() * die);
+        sum += roll;
+    }
+    return sum;
+}
+window.rollDice = rollDice;
+
+global.rollDice = rollDice;
