f2a077a154f7d86040773495adb0d163c01ae77d
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-09-20 23:06:53 -0400

    Show most recent die value

--- Software/react-canvas-draw/src/Components/CharacterFrame.js
+++ Software/react-canvas-draw/src/Components/CharacterFrame.js
@@ -2,26 +2,25 @@
 
 import { rollDice } from "../Data/DiceRoller";
 
-function CharacterFrame({ character, setCharacter }) {
-    let dievalues = [1,2,3];
+function CharacterFrame({ character, updateCharacter }) {
+    console.log("character.dievalue", character.dievalue );
     return (
         <div className="characterFrame">
             {character.name}
             <button onClick={
                     () => {
                         let values = [];
-                        for (let i = 0; i < 50; i++) {
+                        for (let i = 0; i < 5; i++) {
                             let value = rollDice("2d8");
                             values.push(value);
                         }
-                        values = values.sort((a, b) => a - b);
-                        dievalues = values;
-                        console.log("die roll", values);
-                        character.dievalues = values;
-                        setCharacter(character);
+                        // values = values.sort((a, b) => a - b);
+                        character.dievalue = values[0];
+                        updateCharacter(character);
+                        console.log("die roll", values, character.dievalue );
                     }
                 }>Roll die</button>
-            {character.dievalues}
+            {character.dievalue}
         </div>
     );
 }
