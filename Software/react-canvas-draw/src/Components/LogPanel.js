22e07d03cd20b89d84512d9ccbc1fc9950ef30c9
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-09-23 01:30:25 -0400

    Fix potential react bug

--- Software/react-canvas-draw/src/Components/LogPanel.js
+++ Software/react-canvas-draw/src/Components/LogPanel.js
@@ -7,15 +7,13 @@ import React from "react";
 class LogPanel extends React.Component{
     constructor(props) {
         super(props)
-        this.log = props.log;
     }
 
     render(){
-    console.log("log", this.log);
     return (
         <div className="logPanel">
             {
-                this.log.entryList.map((entry, i) => (
+                this.props.log.entryList.map((entry, i) => (
                     <LogEntryFrame
                         entry={entry}
                         key={`log_entry_${i}`}
