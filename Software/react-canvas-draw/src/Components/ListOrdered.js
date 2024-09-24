500b78e176d880dc7f456c1e8e6e90d5683af0b2
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-09-24 15:41:51 -0400

    autoformat

--- Software/react-canvas-draw/src/Components/ListOrdered.js
+++ Software/react-canvas-draw/src/Components/ListOrdered.js
@@ -8,7 +8,7 @@ import { Component } from "react";
      * @param {*} contentFunc 
      * @param {*} updateFunc 
      */
-class ListOrdered extends Component{
+class ListOrdered extends Component {
     /**
      * 
      * @param {*} arr 
@@ -22,42 +22,42 @@ class ListOrdered extends Component{
         let arr = this.props.arr;
         let contentFunc = this.props.contentFunc;
         let updateFunc = this.props.updateFunc;
-    return (
-        <div>
-            {arr.map((obj, i) => (
-                <div>
-                    {contentFunc(
-                        obj,
-                        i,
-                        (<>
-                            {/* Move down */}
-                            <button onClick={() => {
-                                if (i < arr.length - 1) {
+        return (
+            <div>
+                {arr.map((obj, i) => (
+                    <div>
+                        {contentFunc(
+                            obj,
+                            i,
+                            (<>
+                                {/* Move down */}
+                                <button onClick={() => {
+                                    if (i < arr.length - 1) {
+                                        arr.splice(i, 1);
+                                        arr.splice(i + 1, 0, obj);
+                                        updateFunc(arr);
+                                    }
+                                }}>&darr;</button>
+                                {/* Move up */}
+                                <button onClick={() => {
+                                    if (i > 0) {
+                                        arr.splice(i, 1);
+                                        arr.splice(i - 1, 0, obj);
+                                        updateFunc(arr);
+                                    }
+                                }}>&uarr;</button>
+                                {/* Remove */}
+                                <button onClick={() => {
                                     arr.splice(i, 1);
-                                    arr.splice(i + 1, 0, obj);
                                     updateFunc(arr);
-                                }
-                            }}>&darr;</button>
-                            {/* Move up */}
-                            <button onClick={() => {
-                                if (i > 0) {
-                                    arr.splice(i, 1);
-                                    arr.splice(i - 1, 0, obj);
-                                    updateFunc(arr);
-                                }
-                            }}>&uarr;</button>
-                            {/* Remove */}
-                            <button onClick={() => {
-                                arr.splice(i, 1);
-                                updateFunc(arr);
-                            }}>X</button>
-                        </>
-                        )
-                    )}
-                </div>
-            ))}
-        </div>
-    );
-}
+                                }}>X</button>
+                            </>
+                            )
+                        )}
+                    </div>
+                ))}
+            </div>
+        );
+    }
 }
 export default ListOrdered;
