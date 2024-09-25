c3389493b3470d8f027830dec4f6e6efec1350c1
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-09-25 00:25:09 -0400

    Remove up and down buttons

--- Software/react-canvas-draw/src/Components/ListOrdered.js
+++ Software/react-canvas-draw/src/Components/ListOrdered.js
@@ -75,22 +75,6 @@ class ListOrdered extends Component {
                             obj,
                             i,
                             (<>
-                                {/* Move down */}
-                                <button onClick={() => {
-                                    if (i < arr.length - 1) {
-                                        arr.splice(i, 1);
-                                        arr.splice(i + 1, 0, obj);
-                                        updateFunc(arr);
-                                    }
-                                }}>&darr;</button>
-                                {/* Move up */}
-                                <button onClick={() => {
-                                    if (i > 0) {
-                                        arr.splice(i, 1);
-                                        arr.splice(i - 1, 0, obj);
-                                        updateFunc(arr);
-                                    }
-                                }}>&uarr;</button>
                                 {/* Remove */}
                                 <button onClick={() => {
                                     arr.splice(i, 1);
