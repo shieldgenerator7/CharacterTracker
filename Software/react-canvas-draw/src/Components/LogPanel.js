d85b681700737da4a1a00f99d9d241731def18cd
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-10-05 02:55:30 -0400

    Fix bug: log filter not updating consistently

--- Software/react-canvas-draw/src/Components/LogPanel.js
+++ Software/react-canvas-draw/src/Components/LogPanel.js
@@ -8,31 +8,26 @@ class LogPanel extends React.Component {
     constructor(props) {
         super(props);
         this.panel = React.createRef();
-        this.logList = [];
-        this.filterLog(this.props.game.event);
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         let div = this.panel.current;
         div.scrollTop = div.scrollHeight;
-        this.filterLog(this.props.game.event);
-    }
-
-    filterLog(filter) {
-        this.logList = this.props.log.entryList;
-        if (filter) {
-            let filterList = filter.trim().toLowerCase().split(" ").map(f => f.trim()).filter(f => f);
-            this.logList = this.logList.filter(entry => filterList.every(f => entry.includes(f)));
-        }
     }
 
     render() {
+        let logList = this.props.log.entryList;
+        let filter = this.props.game.event;
+        if (filter) {
+            let filterList = filter.trim().toLowerCase().split(" ").map(f => f.trim()).filter(f => f);
+            logList = logList.filter(entry => filterList.every(f => entry.includes(f)));
+        }
         return (
             <div className="logPanel"
                 ref={this.panel}
             >
                 {
-                    this.logList.slice(-100).map((entry, i) => (
+                    logList.slice(-100).map((entry, i) => (
                         <LogEntryFrame
                             entry={entry}
                             key={`log_entry_${i}`}
