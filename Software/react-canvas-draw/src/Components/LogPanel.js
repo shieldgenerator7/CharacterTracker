6e94437fabe199b1b3cc3eeff292cd8693ee813f
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-10-05 03:53:14 -0400

    Improve look of log count

--- Software/react-canvas-draw/src/Components/LogPanel.js
+++ Software/react-canvas-draw/src/Components/LogPanel.js
@@ -16,18 +16,13 @@ class LogPanel extends React.Component {
     }
 
     render() {
-        let logList = this.props.log.entryList;
-        let filter = this.props.game.event;
-        if (filter) {
-            let filterList = filter.trim().toLowerCase().split(" ").map(f => f.trim()).filter(f => f);
-            logList = logList.filter(entry => filterList.every(f => entry.includes(f)));
-        }
+        
         return (
             <div className="logPanel"
                 ref={this.panel}
             >
                 {
-                    logList.slice(-100).map((entry, i) => (
+                    this.props.logList.map((entry, i) => (
                         <LogEntryFrame
                             entry={entry}
                             key={`log_entry_${i}`}
